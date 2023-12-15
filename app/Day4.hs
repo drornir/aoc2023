deb0d1627428c39bb27906480328bbfe42951d73
dror nir <me@drornir.dev>
2023-12-15 20:39:32 +0200

    day4 is kinda boring and I have bug

--- app/Day4.hs
+++ app/Day4.hs
@@ -1,18 +1,21 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Use zipWith" #-}
+{-# HLINT ignore "Avoid lambda using `infix`" #-}
+{-# HLINT ignore "Use bimap" #-}
 module Main (main) where
 
 import qualified Data.Char as Char
 import qualified Data.List as List
+import qualified Data.Maybe as Maybe
 import qualified Data.Text as Text
 import qualified Debug.Trace as Trace
 
 main :: IO ()
 main =
   do
-    file <- readFile "inputs/day4.txt"
-    print $ part1 $ fileToCards file
+    file <- readFile "inputs/day4_ex.txt"
+    print $ part2 $ fileToCards file
 
 data Card = Card
   { _num :: Integer,
@@ -41,19 +44,57 @@ lineToCard line =
       have = parseListOfInts havePart'
    in Card {_num = n, _wins = wins, _have = have}
 
-part1 :: [Card] -> Integer
-part1 cards = sum $ map cardValue cards
-
-cardValue :: Card -> Integer
-cardValue card =
-  let matches = List.foldl' f [] $ _wins card
-   in ( if not (null matches)
-          then 2 ^ (List.length matches - 1)
-          else 0
-      )
+findMatches :: Card -> [Integer]
+findMatches card = List.foldl' f [] $ _wins card
   where
     f :: [Integer] -> Integer -> [Integer]
     f acc winNum =
       if winNum `List.elem` _have card
         then winNum : acc
         else acc
+
+--
+part2 :: [Card] -> Integer
+part2 [] = 0
+part2 (c : cx) =
+  let n = countWinningCards (c, cx)
+      rest = part2 cx
+   in Trace.traceShow ("part2", _num c, n, rest) (n + rest)
+
+part2' :: [Card] -> Integer
+part2' [] = 0
+part2' (c : cx) = countWinningCards (c, cx)
+
+countWinningCards :: (Card, [Card]) -> Integer
+countWinningCards (current, nextCards) =
+  let matches = findMatches current
+      winnings = length matches
+      newCards = List.take winnings nextCards
+      newCardsZippedWithRest = List.zip newCards $ map (\i -> List.drop i nextCards) [1 .. winnings]
+      -- [ (c, cx) | c <- newCards, cx <- map (\i -> List.drop i nextCards) [1 .. 5]
+      -- ]
+      winningsFromWinnings = sum (map countWinningCards newCardsZippedWithRest)
+   in Trace.traceShow
+        ( "card",
+          _num current,
+          "winnings",
+          winnings,
+          "total",
+          toInteger winnings + winningsFromWinnings,
+          "newCards",
+          map _num newCards,
+          "restNum",
+          map (\(c, cx) -> (_num c, map _num cx)) newCardsZippedWithRest
+        )
+        (toInteger winnings + winningsFromWinnings)
+
+-- 21213
+part1 :: [Card] -> Integer
+part1 cards = sum $ map cardValue1 cards
+
+cardValue1 :: Card -> Integer
+cardValue1 card =
+  let matches = findMatches card
+   in if not (null matches)
+        then 2 ^ (List.length matches - 1)
+        else 0
